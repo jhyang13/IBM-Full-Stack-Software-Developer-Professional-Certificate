5435ecb494b12e3c515788222b4a2d7b093f4e82
Jiahui Yang <jyang753@wisc.edu>
2024-08-23 15:27:12 -0500

    update

---
+++ Developing_Front-End_Apps_with_React/Hello World with React/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function App(props) {
+    const currDate = new Date();
+  
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {currDate.toLocaleDateString()} and the time now is {currDate.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+  
+  export default App;
